77d37399f1b0a18a2ec399fc552e0014f60908dc
Adriana Mirian Mendes Cardoso <adrianamirianmc@gmail.com>
2021-05-25 19:26:20 -0300

    Update env

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -11,6 +11,7 @@ import { ProductEntity } from 'src/context/product/entities/product.entity';
 		username: process.env.DATABASE_USER || 'postgres',
 		password: process.env.DATABASE_PASSWORD || '1234',
 		database: process.env.DATABASE || 'postgres',
+		url: process.env.DATABASE_URL || 'postgres://tcdilzxvoubeig:018745603a072ea59bcab07c6a151b3446d6e491ed6e236e9b8c3220c348ec12@ec2-107-20-153-39.compute-1.amazonaws.com:5432/d7a3gqae13ofs',
 		entities: [CategoryEntity, ProductEntity],
 		synchronize: true,
 		ssl: {
